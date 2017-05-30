772603a19d93f88e93d9a4653b5719d5d7d07b9d
saisaran16 <saransmiles16@gmail.com>
2017-05-30 17:03:02 +0530

    inital

--- data/org.eclipse.birt.data.oda.mongodb/src/org/eclipse/birt/data/oda/mongodb/impl/DataSetMetaData.java
+++ data/org.eclipse.birt.data.oda.mongodb/src/org/eclipse/birt/data/oda/mongodb/impl/DataSetMetaData.java
@@ -47,8 +47,7 @@ public class DataSetMetaData implements IDataSetMetaData
 	 * getDataSourceObjects(java.lang.String, java.lang.String,
 	 * java.lang.String, java.lang.String)
 	 */
-	public IResultSet getDataSourceObjects( String catalog, String schema,
-			String object, String version ) throws OdaException
+	public IResultSet getDataSourceObjects( String catalog, String schema,String object, String version ) throws OdaException
 	{
 		throw new UnsupportedOperationException( );
 	}
@@ -89,7 +88,7 @@ public class DataSetMetaData implements IDataSetMetaData
 	public String getDataSourceProductVersion( ) throws OdaException
 	{
 		return Integer.toString( getDataSourceMajorVersion( ) ) + "." + //$NON-NLS-1$
-				Integer.toString( getDataSourceMinorVersion( ) );
+		Integer.toString( getDataSourceMinorVersion( ) );
 	}
 
 	/*
